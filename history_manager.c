df251f98bbb4343bf1d32818c0c279563f0e54e1
BridgetLeratoT <leratotshabalala146@gmail.com>
2023-04-20 13:03:23 -0700

    information

--- history_manager.c
+++ history_manager.c
@@ -24,7 +24,6 @@ return (buf);
 /**
 * write_history - creates a file, or appends to an existing file
 * @info: the parameter struct
-*
 * Return: 1 on success, else -1
 */
 int write_history(info_t *info)
@@ -50,8 +49,7 @@ return (1);
 /**
  * read_history - reads history from file
  * @info: the parameter struct
- *
- * Return: histcount on success, 0 otherwise
+ *Return: histcount on success, 0 otherwise
  */
 int read_history(info_t *info)
 {
@@ -63,32 +61,22 @@ char *filename = get_history_file(info);
 
 if (!filename)
 return (0);
-
 fd = open(filename, O_RDONLY);
 free(filename);
-
 if (fd == -1)
 return (0);
-
 if (!fstat(fd, &st))
 fsize = st.st_size;
-
 if (fsize < 2)
 return (0);
-
 buf = malloc(sizeof(char) * (fsize + 1));
-
 if (!buf)
 return (0);
-
 rdlen = read(fd, buf, fsize);
 buf[fsize] = 0;
-
 if (rdlen <= 0)
 return (free(buf), 0);
-
 close(fd);
-
 for (i = 0; i < fsize; i++)
 {
 if (buf[i] == '\n')
@@ -98,18 +86,13 @@ build_history_list(info, buf + last, linecount++);
 last = i + 1;
 }
 }
-
 if (last != i)
 build_history_list(info, buf + last, linecount++);
-
 free(buf);
 info->histcount = linecount;
-
 while (info->histcount-- >= HIST_MAX)
 delete_node_at_index(&(info->history), 0);
-
 renumber_history(info);
-
 return (info->histcount);
 }
 /**
@@ -117,7 +100,6 @@ return (info->histcount);
  * @info: Structure containing potential arguments. Used to maintain
  * @buf: buffer
  * @linecount: the history linecount, histcount
- *
  * Return: Always 0
  */
 list_t *create_node(char *str, int linecount);
@@ -127,19 +109,15 @@ int build_history_list(info_t *info, char *buf, int linecount)
 list_t *node = create_node(buf, linecount);
 if (!node)
 return (-1);
-
 if (info->history)
 append_node(info->history, node);
 else
 info->history = node;
-
 return (0);
 }
-
 /**
  * renumber_history - renumbers the history linked list after changes
  * @info: Structure containing potential arguments. Used to maintain
- *
  * Return: the new histcount
  */
 int renumber_history(info_t *info)
@@ -154,5 +132,3 @@ node = node->next;
 info->histcount = histcount;
 return (histcount);
 }
-
-
