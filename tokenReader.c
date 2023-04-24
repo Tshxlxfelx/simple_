c18dfaa5db3a57e3b72fbd4d76ab8f20001ba2b0
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-04-24 03:22:00 -0700

    update 8

--- tokenReader.c
+++ tokenReader.c
@@ -1,52 +1,88 @@
 #include "shell.h"
-
 /**
- * split_string - splits a string into words using a delimiter string
- * @str: the input string
- * @delim: the delimiter string
- *
- * Return: a pointer to an array of strings, or NULL on failure
- */
-char **split_string(char *str, char *delim)
+* **strtow - splits a string into words. Repeat delimiters are ignored
+* @str: the input string
+* @d: the delimeter string
+* Return: a pointer to an array of strings, or NULL on failure
+*/
+char **strtow(char *str, char *d)
 {
-if (str == NULL || delim == NULL)
+int i, a, b, c, numwords = 0;
+char **s;
+if (str == NULL || str[0] == 0)
 return (NULL);
-
-int num_words = 0;
-char **words = NULL;
-char *token = strtok(str, delim);
-
-while (token != NULL)
+if (!d)
+d = " ";
+for (i = 0; str[i] != '\0'; i++)
+if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+numwords++;
+if (numwords == 0)
+return (NULL);
+s = malloc((1 + numwords) *sizeof(char *));
+if (!s)
+return (NULL);
+for (i = 0, a  = 0; a < numwords; a++)
 {
-num_words++;
-words = realloc(words, num_words *sizeof(char *));
-if (words == NULL)
-goto fail;
-
-words[num_words - 1] = strdup(token);
-if (words[num_words - 1] == NULL)
-goto fail;
-
-token = strtok(NULL, delim);
-}
-
-/* Add a NULL at the end of the array */
-num_words++;
-words = realloc(words, num_words *sizeof(char *));
-if (words == NULL)
-goto fail;
-words[num_words - 1] = NULL;
-
-return (words);
-
-fail:
-/* Free the previously allocated words */
-for (int i = 0; i < num_words; i++)
+while (is_delim(str[i], d))
+i++;
+b = 0;
+while (!is_delim(str[i + b], d) && str[i + b])
+b++;
+s[j] = malloc((b + 1) * sizeof(char));
+if (!s[a])
 {
-free(words[i]);
-}
-free(words);
-
+for (b = 0; b < j; b++)
+free(s[b]);
+free(s);
 return (NULL);
 }
-
+for (c = 0; c < b; c++)
+s[a][c] = str[i++];
+s[a][c] = 0;
+}
+s[a] = NULL;
+return (s);
+}
+/**
+* **strtow2 - splits a string into words
+* @str: the input string
+* @d: the delimeter
+* Return: a pointer to an array of strings, or NULL on failure
+*/
+char **strtow2(char *str, char d)
+{
+int i, a, b, c, numwords = 0;
+char **s;
+if (str == NULL || str[0] == 0)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+if ((str[i] != d && str[i + 1] == d) ||
+(str[i] != d && !str[i + 1]) || str[i + 1] == d)
+numwords++;
+if (numwords == 0)
+return (NULL);
+s = malloc((1 + numwords) *sizeof(char *));
+if (!s)
+return (NULL);
+for (i = 0, a = 0; a < numwords; j++)
+{
+while (str[i] == d && str[i] != d)
+i++;
+b = 0;
+while (str[i + b] != d && str[i + b] && str[i + b] != d)
+b++;
+s[a] = malloc((b + 1) * sizeof(char));
+if (!s[a])
+{
+for (b = 0; b < j; b++)
+free(s[b]);
+free(s);
+return (NULL);
+}
+for (c = 0; c < b; c++)
+s[a][c] = str[i++];
+s[a][c] = 0;
+}
+s[a] = NULL;
+return (s);
+}
